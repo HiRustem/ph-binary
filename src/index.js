967a1f56c8022b845dbb954ce38e92a243884a85
rustemxrid <rustemxrid@icloud.com>
2024-05-26 00:52:26 +0300

    bg lazy loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './pages/index.css'
+
+document.addEventListener("DOMContentLoaded", function () {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".bg-animation"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("bg-animation_visible");
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach(function (lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
+});
